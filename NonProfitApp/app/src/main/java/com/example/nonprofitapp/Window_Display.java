3a83c702a45887a9760c60f8a0505e9bbae42959
Lyon, Joe <jlyon@butler.edu>
2020-07-01 21:43:42 -0400

    Window Display Feature

---
+++ NonProfitApp/app/src/main/java/com/example/nonprofitapp/Window_Display.java
@@ -0,0 +1,14 @@
+package com.example.nonprofitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Window_Display extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_window__display);
+    }
+}
