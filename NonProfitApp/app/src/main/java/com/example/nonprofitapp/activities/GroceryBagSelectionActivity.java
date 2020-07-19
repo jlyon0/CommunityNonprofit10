e1ca8e4dd8b3f3c5f121456811c9cbbab9c071de
Andrea Wynn <wynna@wynna-stg4.rose-hulman.edu>
2020-07-19 15:38:11 -0600

    Adding color change for selected radio buttons

--- NonProfitApp/app/src/main/java/com/example/nonprofitapp/activities/GroceryBagSelectionActivity.java
+++ NonProfitApp/app/src/main/java/com/example/nonprofitapp/activities/GroceryBagSelectionActivity.java
@@ -26,9 +26,12 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class GroceryBagSelectionActivity extends AppCompatActivity {
     public static final String SELECTED_BAG = "com.example.nonprofitapp.BAG";
-    private int selected = -1; // TODO pick a valid default value later
+    private final int DEFAULT_BAG = 0; // whatever is in the first spot in the array is the default
+    private int selected = DEFAULT_BAG;
     private final int DEFAULT_FOOD_BANK = -1;
+
     private ArrayList<String> buttonNames; // get these from Firebase
+    private ArrayList<RadioButton> buttons;
     private static final String TAG = GroceryBagSelectionActivity.class.getName();
 
     @Override
@@ -43,6 +46,7 @@ public class GroceryBagSelectionActivity extends AppCompatActivity {
 
         RadioGroup rg = findViewById(R.id.radioGroup);
         buttonNames = new ArrayList<>();
+        buttons = new ArrayList<>();
         // some random test buttons for now
         buttonNames.add("Kosher");
         buttonNames.add("Halal");
@@ -63,11 +67,10 @@ public class GroceryBagSelectionActivity extends AppCompatActivity {
 
             // TODO check out this line
             radioButton.setButtonDrawable(null);
-
-            radioButton.setVisibility(View.VISIBLE);
+            //radioButton.setVisibility(View.VISIBLE);
 
             // set the default value
-            if (buttonNames.get(i).equals("Vegan")) {
+            if (buttonNames.get(i).equals(selected)) {
                 radioButton.setChecked(true);
                 radioButton.setTextColor(Color.BLUE);
             }
@@ -75,6 +78,7 @@ public class GroceryBagSelectionActivity extends AppCompatActivity {
             // TODO check out this line
             //rg.addView(radioButton, childParam1);
             rg.addView(radioButton);
+            buttons.add(radioButton);
         }
 
         // redraw the radio group
@@ -83,8 +87,9 @@ public class GroceryBagSelectionActivity extends AppCompatActivity {
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                // TODO eventually get the bag name somehow
+                buttons.get(selected).setTextColor(Color.BLACK);
                 selected = checkedId;
+                buttons.get(selected).setTextColor(Color.BLUE);
             }
         });
     }
@@ -92,8 +97,7 @@ public class GroceryBagSelectionActivity extends AppCompatActivity {
     /** Called after user logs in as a customer and selects a food bank and grocery bag */
     public void toSelectPickupTime(View view) {
         Intent intent = new Intent(this, PickupDateSelectionActivity.class);
-        // TODO put the info for the selected bag here
-        //intent.putExtra(SELECTED_BAG, bag);
+        intent.putExtra(SELECTED_BAG, buttonNames.get(selected));
         intent.putExtra(MainActivity.FOOD_BANK_BUTTON, getIntent().getStringExtra(MainActivity.FOOD_BANK_BUTTON));
         startActivity(intent);
     }
