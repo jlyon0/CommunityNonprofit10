74a8be1f7c950591b2d6038e330238bd63a4945f
sodonova <sodonova@purdue.edu>
2020-07-19 18:43:08 -0400

    Bag selection bugfix

--- NonProfitApp/app/src/main/java/com/example/nonprofitapp/activities/GroceryBagSelectionActivity.java
+++ NonProfitApp/app/src/main/java/com/example/nonprofitapp/activities/GroceryBagSelectionActivity.java
@@ -2,6 +2,7 @@ package com.example.nonprofitapp.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 
 import com.example.nonprofitapp.DataRepository;
 import com.example.nonprofitapp.R;
+import com.example.nonprofitapp.viewmodels.BagSelectViewModel;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,6 +23,7 @@ public class GroceryBagSelectionActivity extends AppCompatActivity {
     public static final String SELECTED_BAG = "com.example.nonprofitapp.BAG";
     private String bag = "custom"; // default to custom bag
     private final int DEFAULT_FOOD_BANK = -1;
+    BagSelectViewModel viewModel;
     private static final String TAG = GroceryBagSelectionActivity.class.getName();
 
 
@@ -28,10 +31,7 @@ public class GroceryBagSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grocery_bag_selection);
-        FirebaseAuth.getInstance().getCurrentUser().reload();
-        Log.i(TAG, "" + FirebaseAuth.getInstance().getCurrentUser().isEmailVerified());
-        Intent receivedLauncher = getIntent();
-
+        viewModel = ViewModelProviders.of(this).get(BagSelectViewModel.class);
        // int foodBankID = receivedLauncher.getIntExtra(MainActivity.FOOD_BANK_BUTTON, DEFAULT_FOOD_BANK);
 
 
@@ -69,6 +69,7 @@ public class GroceryBagSelectionActivity extends AppCompatActivity {
 
     /** Called after user logs in as a customer and selects a food bank and grocery bag */
     public void toSelectPickupTime(View view) {
+        viewModel.setBag(bag);
         Intent intent = new Intent(this, PickupDateSelectionActivity.class);
         intent.putExtra(SELECTED_BAG, bag);
         intent.putExtra(MainActivity.FOOD_BANK_BUTTON, getIntent().getStringExtra(MainActivity.FOOD_BANK_BUTTON));
